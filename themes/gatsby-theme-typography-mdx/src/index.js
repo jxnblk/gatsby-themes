6a77366376c4fbc1b890221d1c9c0dffc397662b
Brent Jackson <jxnblk@gmail.com>
2019-03-20 14:47:45 -0400

    Add component merging to typography-mdx theme

--- themes/gatsby-theme-typography-mdx/src/index.js
+++ themes/gatsby-theme-typography-mdx/src/index.js
@@ -3,12 +3,25 @@ import { MDXProvider } from '@mdx-js/tag'
 import typographyMDX from 'typography-mdx'
 import theme from './typography'
 
+const mergeComponents = inner => outer => {
+  const components = { ...inner }
+  for (const key in outer) {
+    if (!components[key].withComponent) {
+      components[key] = outer[key]
+      continue
+    }
+    components[key] = components[key].withComponent(outer[key])
+  }
+  return components
+}
+
 const Root = props => {
   const components = useMemo(() => {
     return typographyMDX(props.theme)
   }, [ props.theme ])
+
   return (
-    <MDXProvider components={components}>
+    <MDXProvider components={mergeComponents(components)}>
       {props.children}
     </MDXProvider>
   )
