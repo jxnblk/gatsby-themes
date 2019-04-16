a59e0f5dcabeed983284c7a31a6137ea9def5094
Brent Jackson <jxnblk@gmail.com>
2019-04-16 19:09:32 -0400

    Add basic example for conference theme

--- themes/gatsby-theme-conference/gatsby-node.js
+++ themes/gatsby-theme-conference/gatsby-node.js
@@ -1,7 +1,30 @@
+const fs = require('fs-extra')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const Debug = require('debug')
+const pkg = require('./package.json')
+
+const debug = Debug(pkg.name)
 
 const Landing = require.resolve('./src/templates/index')
 const Speakers = require.resolve('./src/templates/speakers')
 const Schedule = require.resolve('./src/templates/schedule')
+
+exports.onPreBootstrap = ({ store }) => {
+  const { program } = store.getState()
+
+  const dir = path.join(program.directory, `src/data`)
+
+  debug(`Initializing ${dir} directory`)
+  mkdirp.sync(dir)
+
+  // todo: find a better way to bootstrap site data
+  const src = path.join(__dirname, 'src/data')
+  const dest = path.resolve('src/data')
+  if (src !== dest) {
+    fs.copySync(src, dest)
+  }
+}
 
 exports.createPages = async ({ graphql, actions }, opts = {}) => {
   actions.createPage({
