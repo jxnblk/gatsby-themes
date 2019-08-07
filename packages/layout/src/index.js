d7d4971bb84619f7ee97d393a7546afd2878b002
Brent Jackson <jxnblk@gmail.com>
2019-08-07 12:28:59 -0400

    Add wrap option to layout theme

--- packages/layout/src/index.js
+++ packages/layout/src/index.js
@@ -1,4 +1,27 @@
+import minimatch from 'minimatch'
+
 export { default as Layout } from './layout'
 export { default as Header } from './header'
 export { default as Footer } from './footer'
 export { default as Sidebar } from './sidebar'
+
+export const wrapPageElement = ({ element, props }, opts = {}) => {
+  const {
+    wrap = false,
+    matchPath,
+  } = opts
+  if (!opts.wrap) return element
+
+  if (matchPath) {
+    const matches = minimatch(props.location.pathname, matchPath)
+    if (!matches) return element
+  }
+
+  return (
+    <Layout {...props}>
+      <Header />
+      {element}
+      <Footer />
+    </Layout>
+  )
+}
